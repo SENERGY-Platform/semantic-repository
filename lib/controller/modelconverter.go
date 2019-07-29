7dbb0914b60d152069da7506e080e387e0b77718
fjauer <19175201+fjauer@users.noreply.github.com>
2019-07-29 10:12:55 +0200

    delete prints

--- lib/controller/modelconverter.go
+++ lib/controller/modelconverter.go
@@ -6,7 +6,6 @@ import (
 	"github.com/SENERGY-Platform/semantic-repository/lib/model"
 	"github.com/piprate/json-gold/ld"
 	"log"
-	"reflect"
 	"runtime/debug"
 	"strings"
 )
@@ -19,13 +18,11 @@ func (*Controller) RdfXmlToModel(deviceClasses string, result interface{}) (err 
 		return err
 	}
 
-	log.Println("triple:", triples)
-
 	turtle := []string{}
 	for _, triple := range triples {
 		turtle = append(turtle, triple.Serialize(rdf.Turtle))
 	}
-	log.Println("turtle:", turtle)
+
 	proc := ld.NewJsonLdProcessor()
 	options := ld.NewJsonLdOptions("")
 	//options.CompactArrays = false
@@ -36,57 +33,50 @@ func (*Controller) RdfXmlToModel(deviceClasses string, result interface{}) (err 
 		log.Println("Error: FromRDF()", err)
 		return err
 	}
-	log.Println("doc:", doc)
-	log.Println(reflect.TypeOf(doc))
 	contextDoc := map[string]interface{}{
-
-			"rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-			"id": "@id",
-			"type": "@type",
-			"name": "rdfs:label",
-			"functions": model.SES_ONTOLOGY_EXPOSES_FUNCTION,
-		    "concept_ids": model.SES_ONTOLOGY_HAS_CONCEPT,
-	}
-
-	contextFrame := map[string]interface{}{
-
-		"rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-		"id": "@id",
-		"type": "@type",
-		"name": "rdfs:label",
-		"functions": model.SES_ONTOLOGY_EXPOSES_FUNCTION,
+		"rdfs":        "http://www.w3.org/2000/01/rdf-schema#",
+		"id":          "@id",
+		"type":        "@type",
+		"name":        "rdfs:label",
+		"functions":   model.SES_ONTOLOGY_EXPOSES_FUNCTION,
 		"concept_ids": model.SES_ONTOLOGY_HAS_CONCEPT,
 	}
 
-	graph := map[string]interface{} {
+	contextFrame := contextDoc
+
+	graph := map[string]interface{}{
 	}
 	graph["@context"] = contextDoc
 	graph["@graph"] = doc
 
-	cont := map[string]interface{} {}
+	cont := map[string]interface{}{}
 	cont["@context"] = contextFrame
 
-	log.Println("graph:", graph)
-	framedDoc, err := proc.Flatten(graph, cont, options)
+	flattenDoc, err := proc.Flatten(graph, cont, options)
 	if err != nil {
 		debug.PrintStack()
-		log.Println("Error: Frame()", err)
+		log.Println("Error: Flatten()", err)
 		return err
 	}
-	log.Println("framedDoc:", framedDoc)
-	frameDocM, _ := json.Marshal(framedDoc)
-	log.Println("graphNew:", string(frameDocM))
-	xxx, _ := framedDoc.(map[string]interface{})
-	gr := xxx["@graph"].([]interface{})
-	log.Println("gr:", gr)
-	b, err := json.Marshal(gr)
-	log.Println("b:", string(b))
+
+	flattenDocCast, _ := flattenDoc.(map[string]interface{})
+	if err != nil {
+		debug.PrintStack()
+		log.Println("Error: FlattenDoc Cast()", err)
+		return err
+	}
+
+	if flattenDocCast["@graph"] == nil {
+		return nil
+	}
+
+	flattenDocGraph := flattenDocCast["@graph"].([]interface{})
+	b, err := json.Marshal(flattenDocGraph)
 	err = json.Unmarshal(b, &result)
 	if err != nil {
 		debug.PrintStack()
 		log.Println("Error: Unmarshal()", err)
 		return err
 	}
-	log.Println("b:", b)
 	return nil
 }
